e800f1d4e7bc4cb50ec9ef42655b7bd58af562a0
tarzemanovrr <tarzemanovrr@63pokupki.ru>
2023-10-13 16:32:32 +0400

    begin

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import PersonalData from '../views/PersonalData';
+import Identification from '../views/Identification.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'main',
+    component: PersonalData
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/identification',
+    name: 'identification',
+    component: Identification
   }
 ]
 
---
+++ src/views/PersonalData.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="pesonal-data">
+    <h2>Личные данные</h2>
+    <form class="form">
+      <div>
+        <input type="text" placeholder="Фамилия">
+        <input type="text" placeholder="Имя">
+      </div>
+      <div>
+        <input type="text" placeholder="Отчество">
+        <input type="date" placeholder="Дата рождения">
+      </div>
+      <div>
+        <input type="text" placeholder="Страна места рождения">
+        <input type="mail" placeholder="E-mail">
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonalData',
+}
+</script>
+
+<style scoped>
+.form {
+  display: flex;
+  justify-content: space-around;
+}
+.pesonal-data {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+.form div {
+  display: flex;
+  /* gap: 30px; */
+  justify-content: space-around;
+}
+/* input {
+  width: 150px;
+  height: 40px;
+} */
+</style>
